b8c70d5f5555a45ddc3001a17356a24be634421d
Alex Shvarev <alex@shvarev.com>
2021-07-26 12:24:40 +0300

    v0.0 Initial commit

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='AlexFireBot',
     version='0.0',
     description='Бот для AlexFireChat',
+    url='https://github.com/AlexFire-Dev/AlexFireBot',
     packages=['AlexFireBot'],
     install_requires=['requests', 'websockets'],
     python_requires='>3.8',
